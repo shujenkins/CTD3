3d154997aa11a061f1eb74799827e787c22ab3c6
Shu Jenkins <shujenkins@gmail.com>
2018-09-22 04:51:32 +0000

    Initial Commit

---
+++ week_one/reading_assignments/ruby_basics.rb
@@ -0,0 +1,33 @@
+# answer each of the following to the best of your ability
+# there is always more than one way to code a response ??
+# please use your knowledge of variable assignments in each response
+# when you are done, share your code with your instructor, push it to Github, or submit a pull request
+
+
+# output an integer
+
+myNum = 33
+print 'The number ' + myNum.to_s + ' is an integer!'
+puts ''
+puts 'You can output an integer simply by stating below:'
+puts 33
+
+# output the number of seconds in a week
+
+secVar = 60 * 60 * 24 * 7
+puts 'There are ' + secVar.to_s + ' seconds in a week!'
+
+# output a float
+
+myNum2 = 33.5
+puts 'The number ' + myNum2.to_s + ' is a float or floating-point number.'
+
+# An output of a float can be shown with multiplication below
+puts 2.0 * 3.5
+
+# output a string
+
+puts 'Hello, my name is Shu!'
+
+# output concatination of strings
+puts "Taco" + "Bell"
